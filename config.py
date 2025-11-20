f6b335481117d74756c31dabe36db1941efb9888
Spiritual-Programmer <kamaljot8165@gmail.com>
2025-11-20 01:13:10 -0800

    update link in config

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 from pathlib import Path
 
 CURRENT_DIR = Path(__file__).parent
-FORM_1040_TEMPLATE_PATH = CURRENT_DIR / "./backend/form_1040_template.pdf"
+FORM_1040_TEMPLATE_PATH = CURRENT_DIR / "backend" / "form_1040_template.pdf"
